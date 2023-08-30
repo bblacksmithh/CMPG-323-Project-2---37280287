dabd51df7d51bd974983d579d5e59cb6ece3b865
blacksmith <101953303+bblacksmithh@users.noreply.github.com>
2023-08-30 17:24:00 +0200

    Added Controllers

---
+++ EcoPowerLogisticsAPI_37280287/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EcoPowerLogisticsAPI_37280287.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+
+        public short ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public string? ProductDescription { get; set; }
+        public int? UnitsInStock { get; set; }
+
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
